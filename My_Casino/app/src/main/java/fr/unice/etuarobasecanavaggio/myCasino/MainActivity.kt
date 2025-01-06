a37a525de93ac1e228a9074458ed3d75b3a52740
Canavaggio-Thibault <visiteuroffutur29@yahoo.fr>
2025-01-06 10:22:11 +0100

    My_Casino

---
+++ My_Casino/app/src/main/java/fr/unice/etuarobasecanavaggio/myCasino/MainActivity.kt
@@ -0,0 +1,37 @@
+package fr.unice.etuarobasecanavaggio.myCasino
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // Configuration du padding pour Edge-to-Edge
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Configuration du bouton de lancer de dé
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = (1..6).random()
+        resultText.text = randomInt.toString()
+
+        Toast.makeText(this, "Dé lancé!", Toast.LENGTH_SHORT).show()
+    }
+}
